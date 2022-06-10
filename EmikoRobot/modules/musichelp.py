c6a5f9bd21afa1cfe7f8eb19351b870564ac0dde
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-06-10 09:22:55 +0530

    Create musichelp.py

---
+++ EmikoRobot/modules/musichelp.py
@@ -0,0 +1,19 @@
+__mod_name__ = "Musicplayer"
+
+__help__ = """
+──「 Music player 」──              
+
+❂ /play (query or reply audio) to playing music from youtube or your audio telegram.
+❂ /vplay (query or reply video) to playing music from youtube or your video telegram.
+❂ /playlist (choose option) for playing your personal playlist or group playlist.
+
+• Admins only
+
+❂ /auth - Add to authorized users.
+❂ /unauth - Canceling or deleting from authorized users list.
+❂ /skip - Play next song.
+❂ /pause - Pause the currently playing song.
+❂ /resume - Resume the paused song.
+❂ /end - stop the currently playing music.
+❂ /settings - Get settings dashboard.
+"""
